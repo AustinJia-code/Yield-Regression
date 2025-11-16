c0181ac0dd38291917147d26b3347e3742ec8dac
AustinJia-code <email.austin.jia@gmail.com>
2025-11-16 00:20:26 -0600

    4.21 RF kaggle

--- preprocessing.py
+++ preprocessing.py
@@ -2,34 +2,52 @@ from typing import Tuple
 import pandas as pd
 import numpy as np
 from scipy.stats import zscore
+from enum import Enum
+
+class RecordType (Enum):
+    KEEP = 0
+    DROP = 1
 
 ###### PARAMS ######
 # Data Cleaning
 LABEL_COL = 'Milk_Yield_L'
-ALL_FEATURES = ["Cattle_ID", "Breed", "Climate_Zone", "Management_System", 
-                "Age_Months", "Weight_kg", "Parity", "Lactation_Stage", 
-                "Days_in_Milk", "Feed_Type", "Feed_Quantity_kg",
-                "Feeding_Frequency", "Water_Intake_L", "Walking_Distance_km", 
-                "Grazing_Duration_hrs", "Rumination_Time_hrs", "Resting_Hours",
-                "Ambient_Temperature_C", "Humidity_percent", "Housing_Score",
-                "FMD_Vaccine", "Brucellosis_Vaccine", "HS_Vaccine", 
-                "BQ_Vaccine", "Anthrax_Vaccine", "IBR_Vaccine", "BVD_Vaccine",
-                "Rabies_Vaccine", "Previous_Week_Avg_Yield", 
-                "Body_Condition_Score", "Milking_Interval_hrs", "Date",
-                "Farm_ID", "Feed_Quantity_lb", "Mastitis", "Milk_Yield_L"]
+NUM_FEATURES = {'Cattle_ID':                    RecordType.DROP,
+                'Age_Months':                   RecordType.KEEP,
+                'Weight_kg':                    RecordType.KEEP,
+                'Parity':                       RecordType.KEEP,
+                'Days_in_Milk':                 RecordType.KEEP,
+                'Feed_Quantity_kg':             RecordType.DROP,
+                'Feed_Quantity_lb':             RecordType.KEEP,
+                'Feeding_Frequency':            RecordType.DROP,
+                'Water_Intake_L':               RecordType.KEEP,
+                'Walking_Distance_km':          RecordType.DROP, 
+                'Grazing_Duration_hrs':         RecordType.DROP,
+                'Rumination_Time_hrs':          RecordType.KEEP,
+                'Resting_Hours':                RecordType.DROP,
+                'Ambient_Temperature_C':        RecordType.KEEP,
+                'Humidity_percent':             RecordType.DROP,
+                'Housing_Score':                RecordType.DROP,
+                'FMD_Vaccine':                  RecordType.DROP,
+                'Brucellosis_Vaccine':          RecordType.DROP, 
+                'HS_Vaccine':                   RecordType.DROP, 
+                'BQ_Vaccine':                   RecordType.DROP,
+                'Anthrax_Vaccine':              RecordType.KEEP,
+                'IBR_Vaccine':                  RecordType.KEEP,
+                'BVD_Vaccine':                  RecordType.DROP,
+                'Rabies_Vaccine':               RecordType.KEEP,
+                'Previous_Week_Avg_Yield':      RecordType.KEEP, 
+                'Body_Condition_Score':         RecordType.DROP,
+                'Milking_Interval_hrs':         RecordType.KEEP,
+                'Date':                         RecordType.KEEP,
+                'Farm_ID':                      RecordType.DROP,
+                'Mastitis':                     RecordType.KEEP,}
 
-DROP_FEATURES = ['Cattle_ID',
-                 'Feed_Quantity_kg',
-                 'Farm_ID',                # Too many options for one-hot
-                 ]
-
-IMPUTE_FEATURES = ['Feed_Quantity_lb',     # Missing 10k
-                 'Housing_Score'           # Missing 6k
-                 ]
-
-# Feature Engineering
-CATEGORICAL_FEATURES = ['Breed', 'Climate_Zone', 'Management_System',
-                        'Feed_Type', 'Lactation_Stage']
+# Non-Dropped Categorical Features
+CAT_FEATURES = {'Breed':                        RecordType.DROP,
+                'Climate_Zone':                 RecordType.DROP,
+                'Management_System':            RecordType.DROP,
+                'Lactation_Stage':              RecordType.KEEP,
+                'Feed_Type':                    RecordType.DROP,}
 
 
 ######## FUNCTIONS ########
@@ -44,11 +62,22 @@ def clean_data (
     labels = None
 
     # Drop useless features
-    cleaned_data = raw_data.drop (DROP_FEATURES,
-                                  axis = 1)
+    drop_fts = ([ft for ft, t in NUM_FEATURES.items () if t == RecordType.DROP] +
+                [ft for ft, t in CAT_FEATURES.items () if t == RecordType.DROP])
+    
+    cleaned_data = raw_data.drop (drop_fts, axis = 1)
+    
+    # # Fix typos
+    # cleaned_data["Breed"] = cleaned_data["Breed"].replace ({
+    #                                 "Holstien": "Holstein",
+    #                                 " Brown Swiss": "Brown Swiss",
+    #                                 "Brown Swiss ": "Brown Swiss"})
     
     # Further processing for training set
     if is_train or LABEL_COL in cleaned_data:
+        # Remove noise
+        cleaned_data = cleaned_data[cleaned_data[LABEL_COL] >= 0]
+
         # Get labels
         labels = cleaned_data[LABEL_COL].values.ravel ()
         cleaned_data = cleaned_data.drop (LABEL_COL, axis = 1)
@@ -58,32 +87,43 @@ def clean_data (
 
 
 def engineer_data (
-    cleaned_data: pd.DataFrame,
-    is_train: bool = False
+    cleaned_data: pd.DataFrame
 ) -> pd.DataFrame:
     """
     Feature Engineering
     Returns engineered dataframe
     """
-    # Convert date to monthly circular representation
+    # Convert date to season
     months = pd.to_datetime (cleaned_data['Date']).dt.month
     engineered_data = cleaned_data.drop (columns = ['Date'])
-    engineered_data['Month_sin'] = np.sin (2 * np.pi * months / 12)
-    engineered_data['Month_cos'] = np.cos (2 * np.pi * months / 12)
 
-    # One Hot encode
-    engineered_data = pd.get_dummies (engineered_data,
-                                      columns = CATEGORICAL_FEATURES,
-                                      drop_first = True)
+    def month_to_season (m):
+        if m in [12, 1, 2]:
+            return "Winter"
+        elif m in [3, 4, 5]:
+            return "Spring"
+        elif m in [6, 7, 8]:
+            return "Summer"
+        else:
+            return "Fall"
+        
+    engineered_data['Season'] = months.apply (month_to_season)
 
     # Impute
-    if is_train:
-        # Drop records w/ missing cols
-        engineered_data = engineered_data.dropna ()
+    impute_fts = [col for col in engineered_data.columns 
+                  if engineered_data[col].isna ().sum () > 0]
+    for col in impute_fts:
+        engineered_data[col] = engineered_data[col].fillna (
+                                                engineered_data[col].median ())
 
-        # Drop records w/ outlier labels (z-score)
-        # z = np.abs (zscore (engineered_data[LABEL_COL]))
-        # engineered_data = engineered_data[z < 3]
+    # One Hot encode
+    onehot_fts = [ft for ft, t in CAT_FEATURES.items () if t == RecordType.KEEP]
+    engineered_data = pd.get_dummies (engineered_data,
+                                      columns = onehot_fts + ['Season'],
+                                      drop_first = True)
+
+    # Drop records w/ missing cols
+    engineered_data = engineered_data.dropna ()
 
     return engineered_data
 
